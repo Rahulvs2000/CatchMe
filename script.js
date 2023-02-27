934d335ce6005b4468e284fa19a7c2c7dc5a945e
Rahulvs2000 <66429511+Rahulvs2000@users.noreply.github.com>
2023-02-27 12:16:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function move(){
+  document.getElementById("select").style.marginLeft = "300px";
+  document.getElementById("select").style.marginTop = "100px";
+
+
+}
+
+function back(){
+  document.getElementById("select").style.marginLeft = "600px";
+  document.getElementById("select").style.marginTop = "0px";
+
+}
